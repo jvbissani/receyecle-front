a0ac46038be41bf8995962d3b32085001cee1234
gbonescima@gmail.com <gbonescima@gmail.com>
2024-12-02 02:25:18 -0300

    Code CleanUP

--- src/layouts/app-layout/index.js
+++ src/layouts/app-layout/index.js
@@ -5,21 +5,12 @@ import SideNav from "../../components/layout-components/SideNav";
 import HeaderNav from "../../components/layout-components/HeaderNav";
 import Footer from "../../components/layout-components/Footer";
 import Dashboard from "../../views/dashboard";
-import Estoque from "../../views/estoque";
-import ListaEstoque from "../../views/lista-estoque";
-import DetalhesEstoque from "../../views/detalhes-estoque";
-import ProdutosEstoque from "../../views/produtos-estoque";
 import ListaUsuarios from "../../views/lista-usuarios";
 import CadastroUsuario from "../../views/usuario";
 import ListaProduto from "../../views/lista-produtos";
 import Produto from "../../views/produto";
-import MovimentacaoEstoque from "../../views/movimentacao-estoque";
-import ListaCategoria from "../../views/lista-categoria-produto";
-import Categoria from "../../views/categoria-produto";
-import ListaMovimentacaoEstoque from "../../views/lista-movimentacao-estoque";
 import ListaEmpresa from "../../views/lista-empresa";
 import Empresa from "../../views/empresa";
-import EstoqueProduto from "../../views/lista-estoque-produto";
 
 const { Content } = Layout;
 
@@ -34,20 +25,9 @@ const AppLayout = () => {
 				<Layout className="site-layout">
 					<Content style={{ margin: "0 16px" }}>
 						<Routes>
-							<Route path="/home" element={<Dashboard />} />
-							<Route
-								path="/lista-estoque"
-								element={<ListaEstoque />}
-							/>
-							<Route path="/estoque" element={<Estoque />} />
-							<Route path="/estoque/:id" element={<Estoque />} />
-							<Route
-								path="/detalhes-estoque/:id"
-								element={<DetalhesEstoque />}
-							/>
-							<Route
-								path="/produtos-estoque/:id"
-								element={<ProdutosEstoque />}
+						<Route 
+								path="/home" 
+								element={<Dashboard />} 
 							/>
 							<Route
 								path="/lista-usuarios"
@@ -65,36 +45,26 @@ const AppLayout = () => {
 								path="/lista-produtos"
 								element={<ListaProduto />}
 							/>
-							<Route path="/produto" element={<Produto />} />
-							<Route path="/produto/:id" element={<Produto />} />
-							<Route
-								path="/movimentacao-estoque"
-								element={<MovimentacaoEstoque />}
+							<Route 
+								path="/produto" 
+								element={<Produto />} 
 							/>
-							<Route
-								path="/movimentacao-estoque/:id"
-								element={<MovimentacaoEstoque />}
+							<Route 
+								path="/produto/:id" 
+								element={<Produto />} 
 							/>
-							<Route
-								path="/lista-movimentacao-estoque/:id_estoque"
-								element={<ListaMovimentacaoEstoque />}
-							/>
-							<Route
-								path="/lista-categoria-produto"
-								element={<ListaCategoria />}
-							/>
-							<Route path="/categoria-produto" element={<Categoria />} />
-							<Route path="/categoria-produto/:id" element={<Categoria />} />
 							<Route
 								path="/lista-empresa"
 								element={<ListaEmpresa />}
 							/>
-							<Route path="/empresa" element={<Empresa />} />
-							<Route path="/empresa/:id" element={<Empresa />} />
-							<Route
-								path="/estoque-produto/:id"
-								element={<EstoqueProduto />}
-							/>	
+							<Route 
+								path="/empresa" 
+								element={<Empresa />} 
+							/>
+							<Route 
+								path="/empresa/:id"
+								element={<Empresa />} 
+							/>
 						</Routes>
 					</Content>
 					<Footer />
